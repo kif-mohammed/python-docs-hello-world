01bc49a32f64091c746786056b4dfb08c1a381c6
kif-mohammed <85088553+kif-mohammed@users.noreply.github.com>
2021-06-16 17:17:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 from flask import Flask, request, make_response
 '''
 
-from flask import Flask,request,json,jsonify
+from flask import Flask,request,json
 app = Flask(__name__)
 
 @app.route("/")
@@ -19,10 +19,10 @@ companies = [{"id": 1, "name": "Company One"}, {"id": 2, "name": "Company Two"}]
 def get_companies():
   return json.dumps(companies)
 
-@app.route('/simulate',methods=['POST'])
-def recommend():
-    df_result=get_recommendation(request)
-    return(jsonify(df_result.to_json(orient='records')))
+#@app.route('/simulate',methods=['POST'])
+#def recommend():
+#    df_result=get_recommendation(request)
+#    return(jsonify(df_result.to_json(orient='records')))
 #     return recommendation(request)
 if __name__=='__main__':
     app.run(debug=True)
